2dcd5b4e6af115cf43f517a6b0634dc70bdb4795
= <=>
2020-11-17 16:43:42 +0700

    build method convert tree to sentence, split token from tree, load dataset, get list vocabularies

--- preprocess_data/preprocess_data.py
+++ preprocess_data/preprocess_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import os
+from nltk.tree import Tree
+from nltk.tokenize import word_tokenize
 
 
 class PreprocessData:
@@ -12,6 +14,7 @@ class PreprocessData:
         """
         self.folder_path = FOLDER_PATH
         self.dataset = None
+        self.lib_tokens = None
 
     def load_dataset(self,
                      type_dataset,
@@ -29,7 +32,8 @@ class PreprocessData:
             check_exist = os.path.isfile(DATASET_REQ_PATH)
             if check_exist:
                 with open(DATASET_REQ_PATH, 'r') as reader:
-                    self.dataset = np.array([line.rstrip("\n") for line in reader])
+                    self.dataset = np.array(
+                        [line.rstrip("\n") for line in reader])
                 return self.dataset
             else:
                 raise FileExistsError('File nay ko ton tai')
@@ -39,36 +43,49 @@ class PreprocessData:
 
     def SplitToken_FromTreebank(self,
                                 treebank):
-        pass
+        """Split list các token từ TreeBank
+            
+            treebank - type string
+            return array = [token, token, token,...]
+        """
+        tree = Tree.fromstring(str(treebank))
+        return tree.leaves()
 
     def Tree_toSentence(self,
                         treebank):
-        # with open('./data/trees/train.txt', 'r') as f:
-        #     li_sentence = []
-        #     for line in f:
-        #         li_sentence.append(line.rstrip('\n'))
-        # from nltk.tree import Tree
-        # t = Tree.fromstring(str(li_sentence[0]))
-        # sent = ' '.join(t.leaves())
-        # print(sent)
-        pass
+        """Convert tree thành một sentence hoàn chỉnh với
+
+            treebank - type string
+            return sentence
+        """
+        sentence = ' '.join(self.SplitToken_FromTreebank(treebank))
+        return sentence
+
+    def getAllTokens(self):
+        """Tạo một kho các tokens từ list các sentences
+
+            return dictionary{token: ids, token: ids,...}
+        """
+        big_sent = ' '.join(self.Tree_toSentence(sentence) for sentence in self.dataset)
+        li_tokens = word_tokenize(big_sent)
+        self.lib_tokens = {}
+        ids = 1
+        for token in li_tokens:
+            if token not in self.lib_tokens:
+                self.lib_tokens[token] = ids
+                ids = ids + 1
+        return self.lib_tokens
+        
 
     def assign_sentiment(self,
                          file_phrases,
                          file_sentimentLabels):
         """Gán sentiment labels cho các phrases tương ứng
 
-            Kết quả là 1 DataFrame được lưu thành file csv
+            Kết quả là 1 DataFrame được lưu thành file csv với định dạng
+                row = samples
+                column = ['phrase ids', 'phrases', 'sentiment values']
 
-            Giá trị trả về là 5 DataFrame được phân loại thành 5 class tương ứng theo giá trị sentiment
-
-                very_negative = [0, 0.2]
-                negative = (0.2, 0.4]
-                neutral = (0.4, 0.6]
-                positive = (0.6, 0.8]
-                very_positive = (0.8, 1.0]
-
-                return (very_negative, negative, neutral, positive, very_positive)
         """
         df_phrases = pd.read_csv(file_phrases, sep='|', header=None)
         df_sentimentLabels = pd.read_csv(file_sentimentLabels, sep='|')
@@ -79,26 +96,6 @@ class PreprocessData:
             df_phrases, df_sentimentLabels, on='phrase ids')
         df_assignLabels = df_assignLabels[[
             'phrase ids', 'phrases', 'sentiment values']]
-
-        # class phrases have very negative sentiment
-        very_negative_phrases = df_assignLabels.loc[(df_assignLabels['sentiment values'] >= 0.0) & (
-            df_assignLabels['sentiment values'] <= 0.2)]
-
-        # class phrases have negative sentiment
-        negative_phrases = df_assignLabels.loc[(df_assignLabels['sentiment values'] > 0.2) & (
-            df_assignLabels['sentiment values'] <= 0.4)]
-
-        # class phrases have neutral sentiment
-        neutral_phrases = df_assignLabels.loc[(df_assignLabels['sentiment values'] > 0.4) & (
-            df_assignLabels['sentiment values'] <= 0.6)]
-
-        # class phrases have positive sentiment
-        positive_phrases = df_assignLabels.loc[(df_assignLabels['sentiment values'] > 0.6) & (
-            df_assignLabels['sentiment values'] <= 0.8)]
-
-        # class phrases have very positive sentiment
-        very_positive_phrases = df_assignLabels.loc[(df_assignLabels['sentiment values'] > 0.8) & (
-            df_assignLabels['sentiment values'] <= 1.0)]
 
         # create and save data to folder
         try:
@@ -114,11 +111,10 @@ class PreprocessData:
         finally:
             print(f'Create and save file data at {path}')
             df_assignLabels.to_csv(path + "\\phrases_and_sentiment.csv")
-            return (very_negative_phrases, negative_phrases, neutral_phrases, positive_phrases, very_positive_phrases)
 
     def split_dataset(self,
-                      file_dataSentence,
-                      file_labelSplitData):
+                      SENTENCE_PATH,
+                      LABEL_SENTENCE_PATH):
         """Chia dataset gồm các sentence thành các file data khác nhau dựa theo label, rồi tạo folder để lưu trữ file
         
             label 1 = train data
@@ -126,19 +122,16 @@ class PreprocessData:
             label 3 = dev data
         """
 
-        df_dataSentence = pd.read_csv(file_dataSentence, sep='\t')
-        df_labelSplitData = pd.read_csv(file_labelSplitData, sep=',')
+        df_dataSentence = pd.read_csv(SENTENCE_PATH, sep='\t')
+        df_labelSplitData = pd.read_csv(LABEL_SENTENCE_PATH, sep=',')
         df_mergeSplitLabel = pd.merge(df_dataSentence,
                                       df_labelSplitData,
                                       on='sentence_index')
 
         grp_splitlabel = df_mergeSplitLabel.groupby('splitset_label')
-        train_data = grp_splitlabel.get_group(
-            1).loc[:, ['sentence', 'splitset_label']]
-        test_data = grp_splitlabel.get_group(
-            2).loc[:, ['sentence', 'splitset_label']]
-        dev_data = grp_splitlabel.get_group(
-            3).loc[:, ['sentence', 'splitset_label']]
+        train_data = grp_splitlabel.get_group(1).loc[:, ['sentence', 'splitset_label']]
+        test_data = grp_splitlabel.get_group(2).loc[:, ['sentence', 'splitset_label']]
+        dev_data = grp_splitlabel.get_group(3).loc[:, ['sentence', 'splitset_label']]
 
         datasets = np.array(
             [train_data, test_data, dev_data],
@@ -167,9 +160,14 @@ class PreprocessData:
             dev_data.to_csv(path + "\\dev_data.csv")
             test_data.to_csv(path + "\\test_data.csv")
 
+    
 
 if __name__ == '__main__':
     # demo load dataset
     data = PreprocessData('./data/trees')
     train_data = data.load_dataset('train', '.txt')
-    print(train_data.shape)
+
+    lib_tokens = data.getAllTokens()
+    print(len(lib_tokens))
+
+    
